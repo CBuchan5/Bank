339baac437b7b21085dc97bbe4314014ccec9ccd
Charlotte Buchan <cebuc@talktalk.net>
2022-03-27 17:30:35 +0100

    refactored deposit to start at 0

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,17 @@
 class BankAccount
   
-  def deposit 
- 
-    0
+  attr_reader :start_balance
+
+  def initialize 
+
+    @start_balance = 0
 
   end
+  # def deposit 
+ 
+  #   0
+
+  # end
 
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -4,8 +4,19 @@ describe BankAccount do
   describe 'deposit'
   it 'starts the customer with an account balence of 0' do
     account = BankAccount.new
-    expect(account.deposit).to eq 0
+    expect(account.start_balance).to eq 0
   end
-
+  
+  # it 'allows the customer to top up with a deposit' do 
+  #   account = BankAccount.new
+  #   expect(account.deposit).to eq 5
+  # end
 
 end
+
+# Create the deposit - done
+# Make the deposit defult to 0
+# then focus on top up
+# allow the deposit amout to top_up
+#
+#
