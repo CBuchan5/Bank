515148383ce59822f6762e53ba73c6b49a818164
Charlotte Buchan <cebuc@talktalk.net>
2022-03-29 09:51:11 +0100

    tided up syntax

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -5,7 +5,7 @@ require './lib/bank_account'
 describe Bank_Account do
   let(:subject) { Bank_Account.new }
 
-  describe 'deposit'
+  describe '#deposit'
   it 'starts the customer with an account balence of 0' do
     expect(subject.balance).to eq 0
   end
@@ -14,14 +14,14 @@ describe Bank_Account do
     expect { subject.deposit 10.10 }.to change { subject.balance }.by 10.10
   end
 
-  describe 'withdraw' do
+  describe '#withdraw' do
     it 'allows the customer to withdraw from their account' do
       subject.deposit(10)
       expect { subject.withdraw 5 }.to change { subject.balance }.by(-5)
     end
   end
 
-  describe 'total' do
+  describe '#total' do
     it 'should show to toal after any transaction' do
       subject.deposit(10)
       subject.deposit(10)
