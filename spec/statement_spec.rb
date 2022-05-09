43cf80684d88e207105fa5956d80dd0e536e29eb
Charlotte Buchan <cbuchan@Charlottes-MacBook-Air.local>
2022-05-09 21:38:10 +0100

    updated statement_spec.rb

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,10 +1,13 @@
 require './lib/statement.rb'
 describe Statement do 
   let(:subject) { Statement.new }
-  describe '#statement' do
+  describe '#statement_headers' do
     it 'should print the headers' do 
       expect(subject.statement_headers).to eq ("date || credit || debit || balance")
     end
+  end
+  describe 'show_statement' do
+    it
   end
 end
 # need to print the statement
